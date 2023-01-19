c8b0d57d4d893a4d9915db4385329d2336971072
AlekseiPodusko <112948029+AlekseiPodusko@users.noreply.github.com>
2023-01-19 21:08:25 +0200

    Add files via upload

---
+++ SPTV21ShoesDocumentPodusko/src/manager/DataManager.java
@@ -0,0 +1,108 @@
+package manager;
+
+
+import entity.Product; 
+import entity.Customer; 
+import entity.Purchase;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataManager {
+    private final String PRODUCT_FILE = "files/PRODUCT";
+    private final String CUSTOMER_FILE = "files/CUSTOMER";
+    private final String PURCHASE_FILE="files/PURCHASE";
+
+    public DataManager(){
+        File file = new File("files");
+        file.mkdirs();
+    }
+
+    public void saveProduct(List<Product> products) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(PRODUCT_FILE );
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(products);
+            objectOutputStream.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла PRODUCT");
+        } catch (IOException e) {
+            System.out.println("Ошибка Ввода Вывода");
+        }
+    }
+    public List<Product> loadProduct() {
+        List<Product> products = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(PRODUCT_FILE);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            products = (List<Product>) objectInputStream.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла Product");
+        } catch (IOException e) {
+            System.out.println("Ошибка Ввода Вывода");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Не найден класс");
+        }
+        return products;
+    }
+
+    public void saveCustomer(List<Customer> customers) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(CUSTOMER_FILE);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(customers);
+            objectOutputStream.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла CUSTOMER");
+        } catch (IOException e) {
+            System.out.println("Ошибка Ввода Вывода");
+        }
+    }
+    public List<Customer> loadCustomer() {
+        List<Customer> customers = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(CUSTOMER_FILE);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            customers = (List<Customer>) objectInputStream.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла CUSTOMER");
+        } catch (IOException e) {
+            System.out.println("Ошибка Ввода Вывода");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Не найден класс");
+        }
+        return customers;
+    }
+
+    public void savePurchase(List<Purchase> purchases) {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(PURCHASE_FILE);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(purchases);
+            objectOutputStream.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println("Нет файла Purchase");
+        } catch (IOException e) {
+            System.out.println("Ошибка Ввода Вывода");
+        }
+    }
+    public List<Purchase> loadPurchase() {
+        List<Purchase> purchases = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(PURCHASE_FILE);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            purchases = (List<Purchase> ) objectInputStream.readObject();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Нет файла Purchase");
+        } catch (IOException e) {
+            System.out.println("Ошибка Ввода Вывода");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Не найден класс");
+        }
+        return purchases;
+    }
+
+
+
+}
---
+++ SPTV21ShoesDocumentPodusko/src/manager/ProductManager.java
@@ -0,0 +1,38 @@
+
+package manager;
+import entity.Product;
+
+import java.util.Scanner;
+public class ProductManager {
+    private Scanner scanner;
+
+    public ProductManager() {
+        scanner = new Scanner(System.in);
+    }
+    public Product createProducts(){
+        System.out.print("Введите название продукта: " );
+        String productname= scanner.nextLine();
+        System.out.print("Введите ценну продукта: ");
+        int productprice=scanner.nextInt(); scanner.nextLine(); 
+        System.out.print("Введите количество продукта: ");
+        int productQuantity=scanner.nextInt(); scanner.nextLine(); 
+        Product product= createProduct(productname,productprice,productQuantity);
+        return product;
+    }
+    public Product createProduct(String productname,int productprice,int productquantity){
+        Product product= new Product();
+        product.setProductname(productname);
+        product.setProductprice(productprice);
+        product.setProductQuantity(productquantity);
+      
+        return product;
+    }
+    public void printListProducts(Product[] products){
+        for (int i=0; i<products.length;i++){
+            System.out.println(i+1+". Название: "+products[i].getProductname()+", Ценна: "+products[i].getProductPrice()+", Количество: "+products[i].getProductquantity());
+        }
+        System.out.println();
+        
+    }
+    
+}
